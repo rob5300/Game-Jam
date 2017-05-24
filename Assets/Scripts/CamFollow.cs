4e2d7b9ab267599349d88c9a5f40ed5aebde9acf
Robert (PC) <rob5300@hotmail.com>
2017-05-24 16:52:32 +0100

    Basic movement implemented.

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Camera))]
+public class CamFollow : MonoBehaviour {
+
+    public GameObject target;
+    public float lerpfactor = 0.1f;
+    public Vector3 Offset = new Vector3(0, 0.7f, 0);
+
+    public bool basic = true;
+    Vector3 newPosition;
+	
+	void LateUpdate () {
+        newPosition = transform.position - Offset;
+
+	    if (basic) {
+            newPosition = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        }
+        else {
+            newPosition = new Vector3(Mathf.Lerp(newPosition.x, target.transform.position.x, lerpfactor), Mathf.Lerp(newPosition.y, target.transform.position.y, lerpfactor), transform.position.z);
+        }
+
+        transform.position = newPosition + Offset;
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour {
 
-	// Use this for initialization
+    public float MovementMultiplier = 5f;
+
+    Rigidbody2D rb;
+    float Horizontal = 0f;
+    float Vertical = 0f;
+    Vector2 newVelocity;
+
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+
+        newVelocity = new Vector2(Horizontal, Vertical).normalized;
+        newVelocity *= MovementMultiplier;
+
+        rb.velocity = newVelocity;
 	}
 }
