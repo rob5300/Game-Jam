80695ecd80ca452d54b3e4066623adee679b2044
Robert (PC) <rob5300@hotmail.com>
2017-05-25 20:46:42 +0100

    World gen destroys old objects and replaces them above by generating new objects. Added quick dirty nightmare test.

---
+++ Assets/Scripts/Nightmare.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nightmare : MonoBehaviour {
+
+    public float MoveRate = 1f;
+
+	void Update () {
+        transform.Translate(new Vector2(0, MoveRate * Time.deltaTime));
+	}
+
+}
