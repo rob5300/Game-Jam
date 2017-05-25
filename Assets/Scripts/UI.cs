69dee734d751d839716b7725b606bf0425fd7ac4
Robert (PC) <rob5300@hotmail.com>
2017-05-25 19:07:35 +0100

    Start of Items and inventory

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+    public static UI ui;
+
+    public Image Slot0;
+    public Image Slot1;
+    public Image Slot2;
+    public Image Slot3;
+    public Image Slot4;
+
+    public void Start()
+    {
+        if (!ui) ui = this;
+    }
+
+}
